0d90dacd6337dbe84ea1f4d92a4d9963b2e1e0e2
Alexey Melezhik <melezhik@gmail.com>
2022-10-26 12:06:10 -0500

    fix typo

--- utils/run-pipeline.sh
+++ utils/run-pipeline.sh
@@ -5,5 +5,5 @@ sparrowdo \
 --with_sparky \
 --sparrowfile ci.raku \
 --tags scm=https://git.sr.ht/~craftyguy/superd,\
-task_config=$PWD/examples/bash/nested-deps.yaml \
+tasks_config=$PWD/examples/bash/nested-deps.yaml \
 --desc "build 123"
