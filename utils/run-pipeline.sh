3f08ec831bedff5c5f6e24947f5d949c89af1545
Alexey Melezhik <melezhik@gmail.com>
2022-10-26 11:58:21 -0500

    fix how to run

--- utils/run-pipeline.sh
+++ utils/run-pipeline.sh
@@ -4,6 +4,6 @@ sparrowdo \
 --no_sudo \
 --with_sparky \
 --sparrowfile ci.raku \
---tags scm=https://git.sr.ht/~craftyguy/super,\
+--tags scm=https://git.sr.ht/~craftyguy/superd,\
 task_config=$PWD/examples/bash/nested-deps.yaml \
 --desc "build 123"
