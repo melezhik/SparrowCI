568999001be32d0482028a0e3f3305e8460ac526
Alexey Melezhik <melezhik@gmail.com>
2022-10-27 23:46:44 -0500

    disable bootstrap

--- utils/run-pipeline.sh
+++ utils/run-pipeline.sh
@@ -5,6 +5,6 @@ sparrowdo \
 --with_sparky \
 --sparrowfile ci.raku \
 --tags scm=https://git.sr.ht/~craftyguy/superd,\
-sparrowdo_bootstrap=on,\
+sparrowdo_bootstrap=off,\
 tasks_config=$PWD/examples/bash/task.yaml \
 --desc "build 123"
