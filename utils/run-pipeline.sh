e60c100d4f4e0c4b8840cada84398c0f3983f861
Alexey Melezhik <melezhik@gmail.com>
2022-10-28 18:20:57 -0500

    sparrow example

--- utils/run-pipeline.sh
+++ utils/run-pipeline.sh
@@ -4,7 +4,7 @@ sparrowdo \
 --no_sudo \
 --with_sparky \
 --sparrowfile ci.raku \
---tags scm=https://git.sr.ht/~craftyguy/superd,\
+--tags scm=https://github.com/melezhik/Sparrow6.git,\
 sparrowdo_bootstrap=off,\
-tasks_config=$PWD/examples/bash/task.yaml \
+tasks_config=$PWD/examples/raku/sparrow.yaml \
 --desc "build 123"
