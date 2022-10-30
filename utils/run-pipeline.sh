19ca902d21cd06e22880fa9c47f10d2beb54ac2c
Alexey Melezhik <melezhik@gmail.com>
2022-10-29 21:16:52 -0500

    example of multiple raku version using rakubrew

--- utils/run-pipeline.sh
+++ utils/run-pipeline.sh
@@ -4,7 +4,7 @@ sparrowdo \
 --no_sudo \
 --with_sparky \
 --sparrowfile ci.raku \
---tags scm=https://github.com/melezhik/Sparrow6.git,\
+--tags scm=https://github.com/tbrowder/CSV-AutoClass.git,\
 sparrowdo_bootstrap=off,\
-tasks_config=$PWD/examples/raku/sparrow.yaml \
---desc "build 123"
+tasks_config=$PWD/examples/raku/zef-multiple-versions.yaml \
+--desc "sparrowci test"
