e9dc9499e7c4cb5e697b946da75354cdebf5b934
Alexey Melezhik <melezhik@gmail.com>
2023-09-15 16:46:00 +0000

    build alpine image for arch

--- utils/build-docker-multi.sh
+++
@@ -1,2 +0,0 @@
-docker buildx create --name mybuilder --bootstrap --use
-docker buildx build Dockerfiles/ -f Dockerfiles/sparrow.alpine.multi -t melezhik/sparrow:alpine_multi --platform linux/arm64/v8,linux/amd64
